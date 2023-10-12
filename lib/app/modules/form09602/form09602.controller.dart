c7ad1503a7e2bac9e9f7a09b77a911c0eefb2a76
Djonatan Willenz <softwarepisiculture@gmail.com>
2023-10-12 19:55:24 -0300

    ajuste

--- lib/app/modules/form09602/form09602.controller.dart
+++ lib/app/modules/form09602/form09602.controller.dart
@@ -1,16 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mobile/app/data/models/recibo.pagamento.model.dart';
 
 class Form09602Controller extends GetxController {
-  Future<List<ReciboPagamentoVO>> findAllWorkers() async {
-    List<ReciboPagamentoVO> response = [];
+  Future<List<ListTile>> buscarListaRecibosPagamento() async {
+    List<ListTile> response = [];
 
     response.add(
-      ReciboPagamentoVO(
-        competencia: "01/2020",
-        dataPagamento: '06/02/2020',
-        tipoFolha: 'Folha normal',
-        valorLiquido: 1300.89,
+      const ListTile(
+        title: Text("01/2020"),
+        subtitle: Text('06/02/2020'),
       ),
     );
 
