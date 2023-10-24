79722d2d2493c0b8fc6a5e8ce471ecc7cf0aa60e
Djonatan Willenz <softwarepisiculture@gmail.com>
2023-10-23 21:52:40 -0300

    ajustes

--- lib/app/data/models/recibo.pagamento.model.dart
+++ lib/app/data/models/recibo.pagamento.model.dart
@@ -1,13 +1,23 @@
 class ReciboPagamento {
-  String? competencia;
-  String? tipoFolha;
-  String? dataPagamento;
-  double? valorLiquido;
+  int? _id;
+  String? _competencia;
+  String? _tipoCalculo;
+  String? _dataPagamento;
+  double? _valorLiquido;
 
-  ReciboPagamento({
-    this.competencia,
-    this.tipoFolha,
-    this.dataPagamento,
-    this.valorLiquido,
-  });
+  getId() => _id;
+  getCompetencia() => _competencia;
+  getTipoCalculo() => _tipoCalculo;
+  getDataPagamento() => _dataPagamento;
+  getValorLiquido() => _valorLiquido;
+
+  static ReciboPagamento fromJson(Map<String, dynamic> data) {
+    ReciboPagamento response = ReciboPagamento();
+    response._id = data["id"];
+    response._competencia = data["competencia"];
+    response._tipoCalculo = data["tipo_calculo"];
+    response._dataPagamento = data["data_pagamento"];
+    response._valorLiquido = data["valor_liquido"];
+    return response;
+  }
 }
