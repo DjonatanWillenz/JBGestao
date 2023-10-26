a848b4c6b7d319b816d98582afaf5cebbde0bcf8
Djonatan Willenz <softwarepisiculture@gmail.com>
2023-10-25 23:47:52 -0300

    ajustes

--- lib/app/data/models/login.model.dart
+++ lib/app/data/models/login.model.dart
@@ -2,8 +2,8 @@ class Login {
   String? _email;
   String? _senha;
 
-  getEmail() => _email;
-  getSenha() => _senha;
+  getEmail() => _email ?? "";
+  getSenha() => _senha ?? "";
 
   static Login fromJson(Map<String, dynamic> json) {
     Login response = Login();
